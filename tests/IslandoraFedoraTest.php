681a5d561e0f4284438bc88d1ecfb1775337de4c
Mark Jordan <mjordan@sfu.ca>
2014-11-11 14:27:13 -0800

    Added tests for Fedora Commons.

---
+++ tests/IslandoraFedoraTest.php
@@ -0,0 +1,29 @@
+<?php
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+
+class IslandoraFedora extends PHPUnit_Framework_TestCase
+{
+    protected $ini;
+
+    public function setUp()
+    {
+        $this->ini = parse_ini_file('tests.ini', TRUE);
+    }
+
+    public function testSolrIsReady()
+    {
+        $client = new Client();
+        $response = $client->get($this->ini['Fedora']['url']);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testForSolrResponse()
+    {
+        $client = new Client();
+        $response = $client->get($this->ini['Fedora']['url']);
+        // Returned web page's title is "Object Methods HTML Presentation".
+        $this->assertRegExp('/Object\sMethods\sHTML\sPresentation/', (string) $response->getBody());
+    }
+}
+
